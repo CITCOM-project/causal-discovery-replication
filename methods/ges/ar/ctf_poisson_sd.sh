c08709d10feb2d9e4effde52e23ee2361bfbe8a8
Jake Newman <jdnewman1@sheffield.ac.uk>
2026-07-20 14:55:01 +0100

    ges hpc scrips for ar

---
+++ methods/ges/ar/ctf_poisson_sd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=ctf_poisson_sd
+#SBATCH --output=logs/ctf_poisson_sd/output_%A_%a.txt
+#SBATCH --error=logs/ctf_poisson_sd/error_%A_%a.txt
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --array=0-29
+
+source /opt/apps/testapps/common/software/staging/Anaconda3/2024.02-1/etc/profile.d/conda.sh
+conda activate ges_env
+
+RANDOM_SEEDS=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
+
+SEED_INDEX=$(( SLURM_ARRAY_TASK_ID))
+seed=${RANDOM_SEEDS[$SEED_INDEX]}
+
+OUTPUT_FILE="data/ctf/ges_poisson_sd/output_ges_seed_${seed}.dot"
+
+echo "======================================================"
+echo "Master Job ID: $SLURM_ARRAY_JOB_ID | Task ID: $SLURM_ARRAY_TASK_ID"
+echo "Running ges Algorithm with random_seed=${seed}"
+echo "Output will be saved to: ${OUTPUT_FILE}"
+echo "======================================================"
+
+/usr/bin/time -v python ges.py \
+    -d data/ctf/poisson.csv \
+    -o "$OUTPUT_FILE" \
+    --seed "$seed" \
+    -V width num_lines_abs num_shapes_abs num_lines_unit num_shapes_unit height intensity
+
+echo "======================================================"
+echo "Run for random_seed=${seed} completed."
---
+++ methods/ges/ar/ctf_vaccine_sd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=ctf_vaccine_sd
+#SBATCH --output=logs/ctf_vaccine_sd/output_%A_%a.txt
+#SBATCH --error=logs/ctf_vaccine_sd/error_%A_%a.txt
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --array=0-29
+
+source /opt/apps/testapps/common/software/staging/Anaconda3/2024.02-1/etc/profile.d/conda.sh
+conda activate ges_env
+
+RANDOM_SEEDS=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
+
+SEED_INDEX=$(( SLURM_ARRAY_TASK_ID))
+seed=${RANDOM_SEEDS[$SEED_INDEX]}
+
+OUTPUT_FILE="data/ctf/ges_vaccine_sd/output_ges_seed_${seed}.dot"
+
+echo "======================================================"
+echo "Master Job ID: $SLURM_ARRAY_JOB_ID | Task ID: $SLURM_ARRAY_TASK_ID"
+echo "Running ges Algorithm with random_seed=${seed}"
+echo "Output will be saved to: ${OUTPUT_FILE}"
+echo "======================================================"
+
+/usr/bin/time -v python ges.py \
+    -d data/ctf/vaccine.csv \
+    -o "$OUTPUT_FILE" \
+    --seed "$seed" \
+    -V vaccine cum_vaccinations cum_vaccinated cum_infections max_doses
+
+echo "======================================================"
+echo "Run for random_seed=${seed} completed."
