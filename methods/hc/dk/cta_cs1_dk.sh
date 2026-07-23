ab24368cc2e1e5b9b5ab62af4d232fc889b24c40
Jake Newman <jdnewman1@sheffield.ac.uk>
2026-07-23 20:14:56 +0100

    Hill climb domain knowledge scripts

---
+++ methods/hc/dk/cta_cs1_dk.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=cta_cs1_dk
+#SBATCH --output=logs/cta_cs1_dk/output_%A_%a.txt
+#SBATCH --error=logs/cta_cs1_dk/error_%A_%a.txt
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2G
+#SBATCH --array=0-299
+
+source /opt/apps/testapps/common/software/staging/Anaconda3/2024.02-1/etc/profile.d/conda.sh
+conda activate ctf-env
+
+PERCENTS=(1 3 5 10 15 20 25 30 40 50)
+RANDOM_SEEDS=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
+
+PERCENT_INDEX=$(( SLURM_ARRAY_TASK_ID / 30 ))
+SEED_INDEX=$(( SLURM_ARRAY_TASK_ID % 30 ))
+percent=${PERCENTS[$PERCENT_INDEX]}
+seed=${RANDOM_SEEDS[$SEED_INDEX]}
+alpha=0.01
+
+OUTPUT_FILE="data/cta/hc_dk_cs1/output_hc_percent_${percent}_seed_${seed}_alpha_${alpha}.dot"
+
+echo "======================================================"
+echo "Master Job ID: $SLURM_ARRAY_JOB_ID | Task ID: $SLURM_ARRAY_TASK_ID"
+echo "Running HC Algorithm with random_seed=${seed} and percent =${percent}"
+echo "Output will be saved to: ${OUTPUT_FILE}"
+echo "======================================================"
+
+/usr/bin/time -v causal-testing discover \
+    -t HillClimberDiscovery \
+    -d data/cta/cs1_data.csv \
+    -o "$OUTPUT_FILE" \
+    -a $alpha \
+    -e "partials/cta_cs1_percent_${percent}_seed_${seed}_excluded.dot" \
+    -i "partials/cta_cs1_percent_${percent}_seed_${seed}_included.dot" \
+    --technique-kwargs random_seed="$seed" \
+    -V width height intensity num_lines_abs num_shapes_abs num_lines_unit num_shapes_unit
+
+echo "======================================================"
+echo "Run for random_seed=${seed} percent=${percent} completed."
