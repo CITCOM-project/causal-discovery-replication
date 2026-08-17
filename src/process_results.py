1e75b7887479d7fa91cdae3732fdf41dcb0d06cf
Michael Foster <m.foster@sheffield.ac.uk>
2026-08-17 13:32:02 +0100

    Removed temporary normalisation hack and penalising errors

--- src/process_results.py
+++ src/process_results.py
@@ -16,27 +16,13 @@ def read_file(dag_path):
     split = os.path.normpath(os.path.splitext(dag_path)[0]).split(os.sep)
     system = list(takewhile(lambda directory: not directory.startswith("technique-"), split))
     dag = pydot.graph_from_dot_file(dag_path)[0]
-    try:
-        configuration = dict(map(lambda directory: directory.split("-"), split[len(system) :]))
-    except ValueError:
-        pass
+    configuration = dict(map(lambda directory: directory.split("-"), split[len(system) :]))
     configuration = {
         k: float(v) if k not in ["technique", "error"] else v for k, v in (configuration | dag.get_attributes()).items()
     }
     configuration["system"] = system
     configuration["dag_path"] = dag_path
 
-    # Temp hack to normalise test_outcomes
-    causal_dag = CausalDAG(dag_path, ignore_cycles=True)
-    try:
-        causal_dag.datatypes = {node: float for node in causal_dag.nodes}
-        num_tests = len(causal_dag.generate_causal_tests())
-        if num_tests:
-            configuration["pass"] = configuration.get("pass", 0) / num_tests
-            configuration["fail"] = configuration.get("fail", 0) / num_tests
-            configuration["inestimable"] = configuration.get("inestimable", 0) / num_tests
-    except ValueError as e:
-        pass
     return configuration
 
 
@@ -45,24 +31,39 @@ def read_data(data_path: str = None) -> pd.DataFrame:
         data = pd.read_csv(data_path, index_col=0)
     else:
         with Pool() as pool:
-            data = pool.map(read_file, tqdm(glob("results/**/*.dot", recursive=True)))
+            data = pool.map(read_file, tqdm(glob("results_synthetic/**/*.dot", recursive=True)))
         data = pd.DataFrame(data)
+
+        data["directional_sensitivity"] = data["directional_true_positives"] / (
+            data["directional_true_positives"] + data["directional_false_negatives"]
+        )
+        data["non_directional_sensitivity"] = data["non_directional_true_positives"] / (
+            data["non_directional_true_positives"] + data["non_directional_false_negatives"]
+        )
+        data["directional_specificity"] = data["directional_true_negatives"] / (
+            data["directional_true_negatives"] + data["directional_false_positives"]
+        )
+        data["non_directional_specificity"] = data["non_directional_true_negatives"] / (
+            data["non_directional_true_negatives"] + data["non_directional_false_positives"]
+        )
+        data["directional_bcr"] = (data["directional_sensitivity"] + data["directional_specificity"]) / 2
+        data["non_directional_bcr"] = (data["non_directional_sensitivity"] + data["non_directional_specificity"]) / 2
+
+        # Set "pass", "fail", and "inestimable" to NaN if there was an error
+        data.loc[data["error"].notna(), ["pass", "fail", "inestimable"]] = None
+
+        # Set classification metrics to 0 if there was an error
+        data.loc[
+            data["error"].notna(),
+            [
+                f"{directional}_{metric}"
+                for directional in ["directional", "non_directional"]
+                for metric in ["sensitivity", "specificity", "bcr"]
+            ],
+        ] = 0
         if data_path:
             data.to_csv(data_path)
-    data["directional_sensitivity"] = data["directional_true_positives"] / (
-        data["directional_true_positives"] + data["directional_false_negatives"]
-    )
-    data["non_directional_sensitivity"] = data["non_directional_true_positives"] / (
-        data["non_directional_true_positives"] + data["non_directional_false_negatives"]
-    )
-    data["directional_specificity"] = data["directional_true_negatives"] / (
-        data["directional_true_negatives"] + data["directional_false_positives"]
-    )
-    data["non_directional_specificity"] = data["non_directional_true_negatives"] / (
-        data["non_directional_true_negatives"] + data["non_directional_false_positives"]
-    )
-    data["directional_bcr"] = (data["directional_sensitivity"] + data["directional_specificity"]) / 2
-    data["non_directional_bcr"] = (data["non_directional_sensitivity"] + data["non_directional_specificity"]) / 2
+
     return data
 
 
@@ -72,6 +73,7 @@ def plot_accuracy(df: pd.DataFrame, column: str):
     ax.set_xticklabels(df.groupby("technique").groups.keys())
     ax.tick_params("x", rotation=45, rotation_mode="xtick")
     ax.set_title(column.capitalize())
+    ax.set_ylim(0, 1)
     plt.tight_layout()
     plt.savefig(f"figures/{column}.png")
 
@@ -80,7 +82,7 @@ if __name__ == "__main__":
     if not os.path.exists("figures"):
         os.mkdir("figures")
     df = read_data("data.csv")
-    df = df.loc[df["data"] == 1]
+    df = df.loc[df["data"] == 100]
     print(df)
     print(df.dtypes)
     plot_accuracy(df, "pass")
