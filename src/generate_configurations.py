61ba375b027cb31a08e003d7cfcf4eae10bf6c7e
Michael Foster <m.foster@sheffield.ac.uk>
2026-08-27 10:42:34 +0100

    Fixed generate_configurations

--- src/generate_configurations.py
+++ src/generate_configurations.py
@@ -7,11 +7,13 @@ for seed in range(250):
     for technique in techniques:
         random.seed(seed)
         data = random.randint(10, 1000)
-        n_nodes = (random.randint(10, 30),)
-        p_conditional = (random.random(),)
+        n_nodes = random.randint(10, 30)
+        p_conditional = random.random()
         output_file = f"results_synthetic/{technique}/{seed}.dot"
         reference_output_file = f"reference_synthetic/{technique}/{seed}.dot"
-        configurations.append(f"-o {output_file} -t {technique} -D {data} -n {n_nodes} -c {p_conditional} -s {seed}")
+        configurations.append(
+            f"-t {technique} -D {data} -n {n_nodes} -c {p_conditional} -s {seed} -o {output_file} -r {reference_output_file}"
+        )
 
 
 with open("synthetic_configurations.txt", "w") as f:
